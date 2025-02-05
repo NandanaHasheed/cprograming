2219ccf103565e4b0ee5ca1a1f5b3a3dfbdb4651
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2025-02-05 16:23:16 +0530

    Add files via upload

---
+++ bubblesort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+int n,i;
+printf("Enter the no of elements");
+scanf("%d",&n);
+int numbers[n];
+printf("\n Enter the elements");
+for(i=0;i<n;i++){
+scanf("%d",&numbers[i]);
+}
+for(int i=0;i<n-1;i++){
+for(int j=0;j<n-1;j++){
+  if(numbers[j]>numbers[j+1]){
+   int temp=numbers[j];
+   numbers[j]=numbers[j+1];
+   numbers[j+1]=temp;
+   }
+  }
+}
+printf("\n The sorted array is \t");
+for(int i=0;i<n;i++){
+ printf("%d \t ",numbers[i]);
+ }
+ return 0;
+ }
+ 
+ 
+  
+
