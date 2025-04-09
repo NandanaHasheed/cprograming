5014d39968a1d1c57172665b0ba2cc2500a6cec3
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2025-04-09 14:34:41 +0530

    Add files via upload

--- numb.c
+++ numb.c
@@ -1,12 +1,22 @@
 #include<stdio.h>
 int main(){
-  FILE  *fp1,fp2,fp3;
+  FILE  *fp1,*fp2,*fp3;
   fp1=fopen("numbers.txt","r");
+  fp2=fopen("odd.txt","w");
+  fp3=fopen("even.txt","w");
   int num;
   while(fscanf(fp1,"%d",&num)==1){
-   printf("%d\t",num);
+   if(num%2==0){
+     fprintf(fp3,"%d \t",num);
+   }
+   else{
+   fprintf(fp2,"%d \t ",num);
+  }
   }
   fclose(fp1);
+  fclose(fp2);
+  fclose(fp3);
   return 0;
 }
+
 
